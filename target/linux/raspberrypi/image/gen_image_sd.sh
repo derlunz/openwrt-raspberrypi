09946875424fa74433a9edb4a50a272c3a30deb2
Christian Kapeller <christian.kapeller@gmx.net>
2012-04-08 17:59:33 +0200

    sdcard image: build vfat partition with mtools, no need for sudo

--- target/linux/raspberrypi/image/gen_image_sd.sh
+++ target/linux/raspberrypi/image/gen_image_sd.sh
@@ -1,54 +1,36 @@
 #!/usr/bin/env bash
 # Copyright (C) 2006-2012 OpenWrt.org
 set -x 
-[ $# == 5 ] || {
-    echo "usage: $0 <outputfile> <sdcard-size> <bootpart directory> <rootfs size> <rootfs image>"
+[ $# == 4 ] || {
+    echo "usage: $0 <outputfile> <sdcard-size> <bootfs image> <rootfs image>"
     exit 1
 }
 
 OUTPUT="$1"
 SDCARDSIZE="$2"
-KERNELDIR="$3"
-ROOTFSSIZE="$4"
-ROOTFSIMAGE="$5"
+BOOTFSIMAGE="$3"
+ROOTFSIMAGE="$4"
 
-#Clean up
 rm -f "$OUTPUT"
 
 #Calculate needed Size for boot partition
-BOOTSIZE="$(du -sBM $KERNELDIR | cut -f1 )"
-BOOTSIZE="${BOOTSIZE%M}" 
-ROOTFSSIZE=$(( $SDCARDSIZE - $BOOTSIZE))
-
-head=16
-sect=63
-cyl=$(( ($BOOTSIZE + $ROOTFSSIZE) * 1024 * 1024 / ($head * $sect * 512)))
+BOOTSIZE="$(ls -l --block-size=M $BOOTFSIMAGE | cut -d' ' -f5 | tr -d 'M')"
+ROOTFSSIZE="$(ls -l --block-size=M $ROOTFSIMAGE | cut -d' ' -f5 | tr -d 'M')"
 
 # create partition table
-set `ptgen -o "$OUTPUT" -h $head -s $sect -t c -p ${BOOTSIZE}m  -t 83 -p ${ROOTFSSIZE}m`
+set `ptgen -v -o "$OUTPUT" -h 16 -s 63 -t c -p ${BOOTSIZE}m -t 83 -p ${ROOTFSSIZE}m`
 
 BOOTOFFSET="$(($1 / 512))"
 BOOTSIZE="$(($2 / 512))"
 ROOTFSOFFSET="$(($3 / 512))"
 ROOTFSSIZE="$(($4 / 512))"
-BOOTBLOCKS="$((($BOOTSIZE / 2) - 1))"
+IMAGESIZE="$(( ($SDCARDSIZE * 1024 * 1024  / 512 ) - 63))"
 
 #Create an empty file of SD card size
-dd if=/dev/zero of="$OUTPUT" bs=512 seek="$ROOTFSOFFSET" conv=notrunc count="$ROOTFSSIZE"
+dd if=/dev/zero of="$OUTPUT" bs=512 seek=63 conv=notrunc count="$IMAGESIZE"
+
+#Paste in the boot partition
+dd if="$BOOTFSIMAGE" of="$OUTPUT" bs=512 seek="$BOOTOFFSET" conv=notrunc
 
 #Paste in the rootfs partition
 dd if="$ROOTFSIMAGE" of="$OUTPUT" bs=512 seek="$ROOTFSOFFSET" conv=notrunc
-
-#Generate the boot partition
-rm -f "$OUTPUT.boot.vfat"
-rm -rf "$OUTPUT.boot.vfat.mount"
-mkdir "$OUTPUT.boot.vfat.mount"
-
-mkfs.vfat -C  "$OUTPUT.boot.vfat" "$BOOTBLOCKS"
-sudo mount -o loop,uid=$USER,gid=$USER "$OUTPUT.boot.vfat" "$OUTPUT.boot.vfat.mount"
-cp -r "$KERNELDIR"/* "$OUTPUT.boot.vfat.mount"
-sudo umount "$OUTPUT.boot.vfat.mount"
-rm -rf "$OUTPUT.boot.vfat.mount"
-
-#Paste in the boot partition
-dd if="$OUTPUT.boot.vfat" of="$OUTPUT" bs=512 seek="$BOOTOFFSET" conv=notrunc
